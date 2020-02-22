fb5c1ff37e55c21f2f83b4256f554e4cb40b09c6
tairoman <ygge95@gmail.com>
2020-02-22 14:48:56 +0100

    Create StaticRectangle

--- Breakout/Game.gd
+++ Breakout/Game.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var static_rectangle = preload("res://StaticRectangle.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
     randomize()
@@ -7,11 +9,26 @@ func _ready():
     $StartPosition.position.y = get_viewport().get_visible_rect().size.y * 0.9
     $Paddel.start($StartPosition.position)
     $StartTimer.start()
+    
+    # Add left, top, and right screen border collision rectangles
+    
+    var screen_rect = get_viewport().get_visible_rect().size
+    
+    var left = static_rectangle.instance()
+    add_child(left)
+    left.create_rect(Rect2(Vector2(-100, 0), Vector2(100, screen_rect.y)))
+
+    var top = static_rectangle.instance()
+    add_child(top)
+    top.create_rect(Rect2(Vector2(0, -100), Vector2(screen_rect.x, 100)))
+    
+    var right = static_rectangle.instance()
+    add_child(right)
+    right.create_rect(Rect2(Vector2(screen_rect.x, 0), Vector2(screen_rect.x + 100, screen_rect.y)))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #    pass
-
 
 func _on_StartTimer_timeout():
     $Ball.start()
---
+++ Breakout/StaticRectangle.gd
@@ -0,0 +1,22 @@
+extends StaticBody2D
+
+export var rect : Rect2
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    update_collision_rect()
+    
+func create_rect(rect : Rect2):
+    self.rect = rect
+    update_collision_rect()
+    
+
+func update_collision_rect():
+    # Set up collision shape
+    $CollisionShape2D.position.x = rect.position.x + rect.size.x / 2
+    $CollisionShape2D.position.y = rect.position.y + rect.size.y / 2
+    
+    $CollisionShape2D.shape.set_extents(Vector2(rect.size.x / 2, rect.size.y / 2))
+
+func _draw():
+    draw_rect(rect, Color.red)
